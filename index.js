6278b3bea63bceb99747be2ee37a3165d0b3faf5
Lugan2004 <visagielugan42@gmail.com>
2024-04-22 15:20:48 +0200

    fixed the styleActiveBoard function

--- index.js
+++ index.js
@@ -115,14 +115,16 @@ function refreshTasksUI() {
 
 // Styles the active board by adding an active class
 // TASK: Fix Bugs
+
+//Fixed the function by fixing the ForEach typo
 function styleActiveBoard(boardName) {
-  document.querySelectorAll('.board-btn').foreach(btn => { 
+  document.querySelectorAll('.board-btn').forEach(btn => { 
     
     if(btn.textContent === boardName) {
-      btn.add('active') 
+      btn.classList.add('active') 
     }
     else {
-      btn.remove('active'); 
+      btn.classList.remove('active'); 
     }
   });
 }
